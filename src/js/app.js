3e6572edb7567eb7270d77e1c286eff67e9c4470
Steve Rydz <steverydz@gmail.com>
2016-10-08 16:37:50 +0100

    Add call to get data

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
+  getResults () {
+    const beerName = document.getElementById('search').value;
+    fetch(`/api/beers/search/${beerName}`)
+      .then(res => res.json())
+      .then(data => console.log('data', data))
+      .catch(error => console.error('error', error));
+  }
+
   render () {
     return (
       <div>
-        <form action="/" method="GET">
-          <label for="search">Search for a beer</label>
-          <input type="search" name="search" id="search" placeholder="e.g. Carlsberg"/>
-          <button className="btn" type="submit">Go!</button>
-        </form>
+        <input type="search" name="search" id="search" placeholder="e.g. Carlsberg"/>
+        <button className="btn" onClick={this.getResults}>Go!</button>
       </div>
     );
   }
