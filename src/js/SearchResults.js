2817635c9a123f9c8ceb85e7ace39133670d5322
Steve Rydz <steverydz@gmail.com>
2016-10-09 00:14:44 +0100

    Add some animations

--- src/js/SearchResults.js
+++ src/js/SearchResults.js
@@ -4,12 +4,22 @@ import {render} from 'react-dom';
 class SearchResults extends React.Component {
   render() {
     if (this.props && this.props.results && this.props.results.length) {
+      let beerIndex = 0;
+      let animationDelay = 0.1;
+      let animationDelayInc = 0.1;
       return (
         <div>
           {
             this.props.results.map((beer) => {
+              beerIndex++;
+              animationDelay = animationDelay + animationDelayInc;
+
+              let divStyle = {
+                animationDelay: animationDelay + 's'
+              };
+
               return (
-                <div className="search-result" key={beer.company.id}>
+                <div className="search-result fade-in" style={divStyle} key={beer.company.id}>
                   <div>{beer.company.company_name}</div>
                   <div>{beer.company.status}</div>
                 </div>
