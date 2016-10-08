caf0a19411261a7169a97cc18daf4d6e24610b24
Steve Rydz <steverydz@gmail.com>
2016-10-08 10:01:41 +0100

    Add initial API endpoints

---
+++ app/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const router = express.Router();
+const app = express();
+const barnivoreBeerApiUrl = 'http://barnivore.com/beer.json';
+const barnivoreSearchApiUrl = 'http://barnivore.com/search.json?keyword=';
+
+router.get('/api/beers', (req, res) => {
+  request(barnivoreBeerApiUrl, (error, response, body) => {
+    if (error) {
+      res.json({
+        error: error
+      });
+
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      res.json({
+        error: {
+          status: response.statusCode
+        }
+      });
+
+      return;
+    }
+
+    res.json({
+      beers: JSON.parse(body)
+    });
+  });
+});
+
+router.get('/api/beers/search/:beer', (req, res) => {
+  request(barnivoreSearchApiUrl + req.params.beer, (error, response, body) => {
+    if (error) {
+      res.json({
+        error: error
+      });
+
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      res.json({
+        error: {
+          status: response.statusCode
+        }
+      });
+
+      return;
+    }
+
+    res.json({
+      beers: JSON.parse(body)
+    });
+  });
+});
+
+app.use('/', router);
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('App is running on port 3000');
+});
