f90b91c5e937eddccb0aa0c21f0f5944e8cdf126
Steve Rydz <steverydz@gmail.com>
2016-10-08 21:14:09 +0100

    Debounce key up event in search

--- src/js/SearchForm.js
+++ src/js/SearchForm.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {debounce} from 'throttle-debounce';
 import SearchResults from './SearchResults';
+
+const debounceTimeout = 500;
 
 class SearchForm extends React.Component {
   constructor(props) {
@@ -9,27 +12,25 @@ class SearchForm extends React.Component {
       value: '',
       results: []
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.getData = debounce(debounceTimeout, this.getData);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  handleChange(e) {
+    this.getData(e.target.value);
+  }
+
+  getData(value) {
+    this.setState({ value: value });
     fetch(`/api/beer?search=${this.state.value.toLowerCase()}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        this.state.results = data.beer;
-      })
-      .catch((error) => {
-        console.error(error)
-      });
+      .then(res => res.json())
+      .then(data => this.state.results = data.beer)
+      .catch(error => console.error(error));
   }
 
   render() {
     return (
       <div className="search-form">
-        <p><input type="search" value={this.state.value} onChange={this.handleChange}/></p>
+        <p><input type="search" onKeyUp={this.handleChange.bind(this)}/></p>
         <div>
           <SearchResults results={this.state.results}/>
         </div>
